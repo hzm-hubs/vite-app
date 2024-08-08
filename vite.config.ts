5382c951a4a3f1ba9da21420a60043b5414cadae
黄泽民 <>
2024-08-08 14:43:30 +0800

    学习一些新的配置axing

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,19 @@ function getViteEnv(mode, target) {
     return loadEnv(mode, process.cwd())[target];
 }
 
+// 转换 index.html 的专用钩子 transformIndexHtml, 在plugins中调用
+const htmlPlugin = () => {
+    return {
+        name: "html-transform",
+        transformIndexHtml(html) {
+            return html.replace(
+                /<title>(.*?)<\/title>/,
+                `<title>Title replaced!</title>`,
+            );
+        },
+    };
+};
+
 export default ({ mode, command }) =>
     defineConfig({
         //项目根目录
@@ -33,10 +46,16 @@ export default ({ mode, command }) =>
             // 代理
             proxy: {},
 
+            // 默认不限制
+            // cors:
+
             // hmr: {
             //     overlay: false,
             // },
         },
+        // preview:{
+        // port: 6002,
+        // }
         resolve: {
             // 设置中间键名
             // 相对于 src 的路径访问如 ../assets/ 可以用 @/assets/
@@ -59,6 +78,8 @@ export default ({ mode, command }) =>
                     // 需要注入的标签列表
                     tags: [
                         {
+                            // 'head-prepend'
+                            // 'head' | 'body' | 'head-prepend' | 'body-prepend'
                             injectTo: "head",
                             tag: "meta",
                             attrs: {
@@ -89,6 +110,9 @@ export default ({ mode, command }) =>
                     //     blue: "#1CC0FF",
                     // },
                     javascriptEnabled: true,
+                    // 添加单个
+                    // additionalData: `$injectedColor: orange;`,
+                    // 添加文件 以下三种方式都行
                     additionalData: `@import url(@/assets/styles/common.less);`,
                     // additionalData: `@import "@/assets/styles/common.less";`,
                     // additionalData: `@import "${pathResolve(
@@ -98,10 +122,23 @@ export default ({ mode, command }) =>
             },
         },
         // .jsx 和 .tsx 文件同样开箱即用。JSX 的转译同样是通过 esbuild。
-        // esbuild: {
-        // 	jsxFactory: "h",
-        // 	jsxFragment: "Fragment",
-        // },
+        esbuild: {
+            jsxFactory: "h",
+            jsxFragment: "Fragment",
+            // include: []
+        },
+
+        build: {
+            // 输出文件夹
+            outDir: "dist",
+            // 静态资源存放位置
+            assetsDir: "assets",
+            // 小于此阈值的导入或引用资源将内联为 base64 编码，以避免额外的 http 请求
+            assetsInlineLimit: 4096,
+        },
+        optimizeDeps: {
+            entries: ["index.html"],
+        },
     });
 
 // export default defineConfig({
