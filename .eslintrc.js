97d1aae2c673ec538b7523dc5f63681eebea6342
hzm <467251648@qq.com>
2025-09-26 17:02:01 +0800

    调整插件

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
 	// 'prettier','plugin:prettier/recommended' 二选一即可，都可以启用 prettier/prettier
-	extends: ["plugin:vue/recommended", "plugin:prettier/recommended"],
+	extends: ["plugin:vue/vue3-recommended", "plugin:prettier/recommended"],
 	rules: {
 		"prettier/prettier": "error",
 		"vue/no-multiple-template-root": "off",
